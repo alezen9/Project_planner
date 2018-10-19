1200d2b2fc51fae6606cea08f6c9da8f80671358
alezen9 <aj0715@live.com>
2018-10-19 03:18:04 +0200

    setting up react and components

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './component/layout/Navbar';
+import Dashboard from './component/dashboard/Dashboard';
+import ProjectDetails from './component/porjects/ProjectDetails';
+import SignIn from './component/auth/SignIn';
+import SignUp from './component/auth/SignUp';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/porjects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project title</span>
+                <p>Posted by aleks</p>
+                <p className="grey-text">18th Oct, 2018</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
